e2af06c85dd7b97038d0c440971b6510f1f66a6d
Leqi <100080774+toodemhard@users.noreply.github.com>
2025-01-05 16:08:15 +0000

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: "/kasatria/"
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        periodic_table: resolve(__dirname, 'periodic_table.html'),
-      },
+    base: "kasatria/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                periodic_table: resolve(__dirname, 'periodic_table.html'),
+            },
+        },
     },
-  },
 })
