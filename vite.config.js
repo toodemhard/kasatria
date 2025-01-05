b857e04354a35adb2e3cc1b4d9801387203fdb01
Leqi <100080774+toodemhard@users.noreply.github.com>
2025-01-05 16:36:43 +0000

    try to fix path again

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: "kasatria/",
+    base: "/kasatria/",
     build: {
         rollupOptions: {
             input: {
